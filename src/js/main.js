1262b9ae95fe6207ff7f01344319de27d95900a4
kashpro <kash083@gmail.com>
2021-10-01 20:38:18 +0800

    raw complete

--- src/js/main.js
+++ src/js/main.js
@@ -2,25 +2,7 @@
 import "@/scss/main.scss";
 
 /* Development stats */
-import Development from './development.js';
+import Development from './modules/development.js';
 if (process.env.NODE_ENV === "development") {Development.addWindowStatsElement();}
 
-/* Preloader */
-import preloader from './preloader.js';
-preloader();
 
-/* Vue app */
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
-
-Vue.config.productionTip = false;
-
-new Vue({
-  router,
-  store,
-  render: h => h(App),
-}).$mount('#vue-app');
-
-console.log(`test5`);
--- src/js/preloader.js
+++
@@ -1,8 +0,0 @@
-const hidePreloader = () => {
-  const preloader = document.querySelector('.js-preloader');
-  preloader.style.display = 'none';
-}
-
-export default () => {
-  window.addEventListener("load", hidePreloader);
-}
